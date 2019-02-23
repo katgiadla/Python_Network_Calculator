8697cdcc6daa11e5e2041ce5e11309009c651ed7
katgiadla <katarzyna.giadla@gmail.com>
2019-02-23 16:45:08 +0100

    Add a users menu

---
+++ Network_Calculator.py
@@ -0,0 +1,24 @@
+def Main():
+    StartOption = True
+    while StartOption == True:
+        print("Choose, what want you to do?")
+        print("\n1. Compute network parametrs of local IP "
+              "\n2. Entry and compute network parametrs"
+              "\n3. Exit ")
+        strChoice = input("Your decision: ")
+        if strChoice.isdigit() == False:
+            print("\nPlease choose 1, 2 or 3")
+            continue
+        else:
+            UserChoice = int(strChoice)
+            if UserChoice not in [1, 2, 3]:
+                print("\nPlease choose 1, 2 or 3")
+            else:
+                if UserChoice == 3:
+                    exit()
+                else:
+                    print("Let's do!")
+                    break
+    return
+
+Main()
